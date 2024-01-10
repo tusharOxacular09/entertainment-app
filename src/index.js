3ca35dfb5d50af4f975a6da9dc3439b6500440d1
Tushar Kanta Swain <143312816+tusharOxacular09@users.noreply.github.com>
2024-01-10 14:42:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://entertainmenthub-tushar09.onrender.com/",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
