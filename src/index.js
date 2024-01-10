fbdec1cde2c98aac88ef78a7babb3f825180421d
Tushar Kanta Swain <143312816+tusharOxacular09@users.noreply.github.com>
2024-01-10 15:26:58 +0530

    update frontend url

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["https://entertainmenthub-tushar-kanta09.onrender.com"],
     credentials: true,
   })
 );
