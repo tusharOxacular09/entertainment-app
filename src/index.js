77baea187f98e28b91467c6301d94247a4ea86e1
Tushar Kanta Swain <143312816+tusharOxacular09@users.noreply.github.com>
2024-01-10 13:00:30 +0530

    changing client url

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://entertainmenthub-tushar09.onrender.com/",
     credentials: true,
   })
 );
